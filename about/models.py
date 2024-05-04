776651a3f18cb934173f2d5c305b62916b6c7299
GrigorySuturin <crack300@yandex.ru>
2024-05-04 14:33:35 +0700

    Project with one app

---
+++ about/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.shortcuts import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(About.Status.PUBLISHED)
+
+
+class About(models.Model):
+    class Status(models.IntegerChoices):
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликовано'
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    content = models.TextField(blank=True)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.PUBLISHED)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('jokes', kwargs={'post_slug': self.slug})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_slug': self.slug})
